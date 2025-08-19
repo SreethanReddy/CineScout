532a5fb555e95d54591cb2129b9abb38dee0cbde
Sreethan Reddy <chintagarisaisreethanreddy@gmail.com>
2025-08-19 10:49:38 +0530

    wrong path error-2 commit

--- app/pages/error.vue
+++ app/pages/error.vue
@@ -1,8 +1,10 @@
 <template>
-    <h1 class="text-xl text-center text-white"> 404 </h1>
-    <p class="text-xl text-center text-white">Page Not Found: {{ route.path }}</p>
-    
-    <NuxtLink to="/" class="text-md text-center text-white"> Go to Home</NuxtLink>
+    <div class="bg-[#242424]">
+        <h1 class="text-xl text-center text-white"> 404 </h1>
+        <p class="text-xl text-center text-white">Page Not Found: {{ route.path }}</p>
+        <NuxtLink to="/" class="text-md text-center text-white"> Go to Home</NuxtLink>
+    </div>
+
 
 </template>
 
